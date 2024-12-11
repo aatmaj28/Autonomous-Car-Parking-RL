4d4a0ca197abb20e4554cae7923a3a0263d06204
Aatmaj Salunke <71608530+aatmaj28@users.noreply.github.com>
2024-12-11 01:18:21 -0500

    Added Unity C# Scripts

---
+++ Unity C#/CarData.cs
@@ -0,0 +1,35 @@
+﻿using AutonomousParking.Car.Wheel;
+using UnityEngine;
+
+namespace AutonomousParking.Car
+{
+    public class CarData : MonoBehaviour
+    {
+        private WheelsData wheelsData;
+        [field: SerializeField] public float MaxEngineTorque { get; private set; } = 100f;
+        [field: SerializeField] public float MaxBrakeTorque { get; private set; } = 1000f;
+        [field: SerializeField, Range(0, 90)] public float MaxSteeringAngle { get; private set; } = 35f;
+        [field: SerializeField, Range(0, 1)] public float DrivetrainEfficiency { get; private set; } = 0.75f;
+
+        public float MaxWheelTorque { get; private set; }
+        public float CurrentWheelTorque { get; set; }
+        public float CurrentSteeringAngle { get; set; }
+        public bool IsBreaking { get; set; }
+
+        private void Awake()
+        {
+            wheelsData = GetComponentInChildren<WheelsData>();
+            int drivingWheelsCount = wheelsData.DrivingWheels.Length;
+
+            MaxWheelTorque = MaxEngineTorque * DrivetrainEfficiency / drivingWheelsCount;
+        }
+
+        public void Reset()
+        {
+            CurrentWheelTorque = default;
+            CurrentSteeringAngle = default;
+            IsBreaking = default;
+            wheelsData.Reset();
+        }
+    }
+}
---
+++ Unity C#/carControl.cs
@@ -0,0 +1,66 @@
+//using System;
+//using System.Collections;
+//using System.Collections.Generic;
+//using UnityEngine;
+
+
+//namespace AutonomousParking
+//{
+
+//    public class carControl : MonoBehaviour
+//    {
+//        [SerializeField] private WheelCollider FrontLeftcollider, FrontRightcollider, RearLeftcollider, RearRightcollider;
+//        // Start is called before the first frame update
+
+//        IEnumerator DelaySeconds()
+//        {
+//            yield return new WaitForSeconds(3f);
+//            FrontLeftcollider.brakeTorque = -50f;
+//            FrontRightcollider.brakeTorque = -50f;
+//            Debug.Log("Brake applied");
+//            // FrontLeftcollider.motorTorque = 30;
+//            // FrontRightcollider.motorTorque = 30;  
+//        }
+//        void Start()
+//        {
+//            FrontLeftcollider.motorTorque = 100;
+//            FrontRightcollider.motorTorque = 100;
+//            // StartCoroutine(DelaySeconds());
+
+//        }
+
+//        public void PerformAction(List<int> action)
+//        {
+//            SetTorque(action[0]);
+//            SetSteerAngle(action[1]);
+//            StopCar(action[2]);
+//        }
+
+//        // Update is called once per frame
+//        void Update()
+//        {
+
+//        }
+
+//        void SetTorque(int torque)
+//        {
+//            RearLeftcollider.motorTorque = torque;
+//            RearRightcollider.motorTorque = torque;
+//        }
+
+//        void SetSteerAngle(int angle)
+//        {
+//            FrontLeftcollider.steerAngle = angle;
+//            FrontRightcollider.steerAngle = angle;
+//        }
+
+//        void StopCar(int flag)
+//        {
+//            if (flag == 1)
+//            {
+//                RearLeftcollider.motorTorque = -3000;
+//                RearRightcollider.motorTorque = -3000;
+//            }
+//        }
+//    }
+//}
